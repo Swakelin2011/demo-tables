85d8ab1e7f2aacd36b55e375de9a631e1a4ac0ea
Wakelin <shw331@pitt.edu>
2025-11-05 11:09:45 -0500

    Fix __init__.py encoding issue

--- demo_tables/__init__.py
+++ demo_tables/__init__.py
@@ -1,1 +1,26 @@
-ÿþ
+ï»¿"""
+Demo Tables Package
+Create demographics tables with summary statistics and statistical tests
+"""
+
+from .demo_tables import (
+    fixed_demographics_table,
+    fixed_grouped_demographics_table,
+    demographics_table_mean_sd,
+    demographics_table_median_iqr,
+    grouped_demographics_table_mean_sd,
+    grouped_demographics_table_median_iqr,
+    quick_fix_demographics
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    'fixed_demographics_table',
+    'fixed_grouped_demographics_table',
+    'demographics_table_mean_sd',
+    'demographics_table_median_iqr',
+    'grouped_demographics_table_mean_sd',
+    'grouped_demographics_table_median_iqr',
+    'quick_fix_demographics'
+]
